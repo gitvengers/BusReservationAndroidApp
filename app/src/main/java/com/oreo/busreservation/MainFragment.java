eb3c55ee08bbc6d264cbfd4cb3aaca3e3896511e
Jeong Jun Lee <rhqnrdl625@naver.com>
2019-12-03 22:47:28 +0900

    design: add login page

--- app/src/main/java/com/oreo/busreservation/MainFragment.java
+++ app/src/main/java/com/oreo/busreservation/MainFragment.java
@@ -1,14 +1,22 @@
 package com.oreo.busreservation;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import butterknife.BindView;
+
 public class MainFragment extends Fragment {
+//    @BindView(R.id.btn_login_into_activity)
+    Button buttonLogin;
+
+
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
@@ -16,6 +24,14 @@ public class MainFragment extends Fragment {
 
         // Snippet from "Navigate to the next Fragment" section goes here.
 
+        buttonLogin = view.findViewById(R.id.btn_login_into_activity);
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
         return view;
     }
 }
