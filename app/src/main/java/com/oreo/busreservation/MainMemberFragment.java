3ca2c7f073e9429c33632e5d2fca6aa6d77c6f7f
Jeong Jun Lee <rhqnrdl625@naver.com>
2019-12-03 20:51:45 +0900

    fix: modify fragment inflater id

--- app/src/main/java/com/oreo/busreservation/MainMemberFragment.java
+++ app/src/main/java/com/oreo/busreservation/MainMemberFragment.java
@@ -11,7 +11,7 @@ import androidx.fragment.app.Fragment;
 public class MainMemberFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        View view = inflater.inflate(R.layout.fragment_main_member, container, false);
         return view;
     }
 }
