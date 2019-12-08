7d1a1a2524a86b027db0777e73292861e4bf303c
rinha7 <eocjstnals12@gmail.com>
2019-12-08 21:47:51 +0900

    Add admin login and BusRegister
    관리자 로그인은 switch를 사용했습니다
    버스 등록은 페이지와 동작만 구현, 이외는 미구현입니다.

--- app/src/main/java/com/oreo/busreservation/domain/Bus.java
+++ app/src/main/java/com/oreo/busreservation/domain/Bus.java
@@ -5,14 +5,28 @@ import java.sql.Timestamp;
 
 //todo 임시 Bus 객체
 public class Bus implements Serializable {
-    private Timestamp departureTime;
     private String departure;
     private String arrival;
+    private Timestamp departureTime;
+    private Timestamp arriveTime;
+    private String type;
+    private String company;
+    private int price;
 
     public Bus(Timestamp departureTime, String departure, String arrival) {
         this.departureTime = departureTime;
         this.departure = departure;
         this.arrival = arrival;
+    }
+
+    public Bus(String departure, String arrival, Timestamp departureTime, Timestamp arriveTime, String type, String company,int price) {
+        this.departure = departure;
+        this.arrival = arrival;
+        this.departureTime = departureTime;
+        this.arriveTime = arriveTime;
+        this.type = type;
+        this.company = company;
+        this.price = price;
     }
 
     public Timestamp getDepartureTime() {
